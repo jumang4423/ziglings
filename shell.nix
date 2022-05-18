eef40563e8c57481ace31c15424add12d848f23c
jumang4423 <catontheskate4423@gmail.com>
2022-05-18 18:21:30 +0900

    addeeed shell.nix

--- shell.nix
+++ shell.nix
@@ -1,19 +1,58 @@
-with import <nixpkgs> {};
+{ lib
+, fetchFromGitHub
+, cmake
+, llvmPackages
+, libxml2
+, zlib
+}:
 
-pkgs.mkShell {
-  nativeBuildInputs = with pkgs; [
+let
+  inherit (llvmPackages) stdenv;
+in
+stdenv.mkDerivation rec {
+  pname = "zig";
+  version = "0.10.0";
+
+  src = fetchFromGitHub {
+    owner = "ziglang";
+    repo = pname;
+    rev = version;
+    hash = "sha256-ad3341fff667f374966f880976f255e5e09fa04ea8a44f5f6a47a28abcc4e291";
+  };
+
+  nativeBuildInputs = [
     cmake
-    gdb
-    ninja
-    qemu
-    wasmtime
-    zig
-  ] ++ (with llvmPackages_13; [
-    clang
-    clang-unwrapped
+    llvmPackages.llvm.dev
+  ];
+
+  buildInputs = [
+    libxml2
+    zlib
+  ] ++ (with llvmPackages; [
+    libclang
     lld
     llvm
   ]);
 
-  hardeningDisable = [ "all" ];
+  preBuild = ''
+    export HOME=$TMPDIR;
+  '';
+
+  doCheck = true;
+  checkPhase = ''
+    runHook preCheck
+    ./zig test --cache-dir "$TMPDIR" -I $src/test $src/test/behavior.zig
+    runHook postCheck
+  '';
+
+  meta = with lib; {
+    homepage = "https://ziglang.org/";
+    description =
+      "General-purpose programming language and toolchain for maintaining robust, optimal, and reusable software";
+    license = licenses.mit;
+    maintainers = with maintainers; [ andrewrk AndersonTorres ];
+    platforms = platforms.unix;
+    broken = stdenv.isDarwin; # See https://github.com/NixOS/nixpkgs/issues/86299
+  };
+}
 }
